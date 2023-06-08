ac3d48b8bb4aaa0eb3b3476822ea015818c51ce9
Bekzhan <bidahmetov2002@gmail.com>
2023-06-08 17:43:41 +0200

    create connection to bd, create middleware func and implement endpoint

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express"); 
+const pgPromise = require("pg-promise"); 
+ 
+const app = express(); 
+const pgp = pgPromise(); 
+ 
+const dbConfig = { 
+  connectionString: 
+    "postgres://xlralosa:8RteJwNO8GNe1dSV_K7YwJCjgIU-QGjW@dumbo.db.elephantsql.com/xlralosa", 
+  ssl: { 
+    rejectUnauthorized: false, 
+  }, 
+}; 
+ 
+const db = pgp(dbConfig); 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const getQuestions = async (amount, category, difficulty) => {
+  let query = `SELECT category, difficulty, question, correct_answer, incorrect_answers FROM Questions WHERE difficulty = $1 AND category = $2 LIMIT $3`;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  return await db.any(query, [difficulty, category, amount]); 
+}
+ 
+app.get("/questions", async (req, res) => { 
+  try { 
+    const { amount, category, difficulty } = req.query;
+    
+    if (!amount || !category || !difficulty) {
+      res.status(500).send("Error executing query");
+    }
+ 
+    const result = await getQuestions(amount, category, difficulty)   
+ 
+    res.json(result); 
+  } catch (error) { 
+    console.error("Error executing query", error); 
+    res.status(500).send("Error executing query"); 
+  } 
+}); 
+ 
+app.listen(3000, () => { 
+  console.log("Server is running on port 3000"); 
+});
